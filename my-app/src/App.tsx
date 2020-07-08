44bc7bb67d30785ab47f4f8e9da555451e079453
lsm <lishumin@imassbank.com>
2020-07-08 14:11:46 +0800

    初始化

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../src/common/images/logo.svg';
+import '../src/common/style/App.css';
 
+import { Fragment } from 'react';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      <div>
+        你好Fragment
+      </div>
+    </Fragment>
   );
 }
 
